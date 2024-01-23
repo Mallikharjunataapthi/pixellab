06614392d060a0448a4f10ae500db8ea6e9c604d
rsravankumar1109 <rsravan@aapthitech.com>
2024-01-23 10:00:27 +0530

    app
    app

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -3,6 +3,9 @@ import { CreateCategoryDto } from './create-category.dto';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+    @IsNotEmpty({ message: 'App is required' })
+    @IsString({ message: 'App must be a string' })
+    readonly app_id:String;
     @IsNotEmpty({ message: 'Category is required' })
     @IsString({ message: 'Category must be a string' })
     readonly cat_name:String;

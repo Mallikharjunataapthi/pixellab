0da1836890dda45ea4b3125d8654c85b05ca5fec
rsravankumar1109 <rsravan@aapthitech.com>
2024-03-04 19:06:12 +0530

    template
    template

--- src/user-templates/user-templates.service.ts
+++ src/user-templates/user-templates.service.ts
@@ -65,7 +65,6 @@ export class UserTemplatesService {
             original_template_id: new Types.ObjectId(createUserTemplateDto.user_temp_original_template_id),
             before_image_url:beforeImageS3Response ?beforeImageS3Response : null,
             after_image_url:afterImageS3Response ? afterImageS3Response : null,
-            template_name: templateData.data.template_name,
             cat_id:templateData.data.cat_id,
             category_name:templateData.data.category_name,
             whishlist_count:0,
